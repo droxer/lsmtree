7de048b6dca2fec4af432bf824316d517df235ed
He Fei <droxer.he@gmail.com>
2017-12-15 23:42:12 +0800

    add testify.

---
+++ stub_test.go
@@ -0,0 +1,12 @@
+package lsmtree
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSomething(t *testing.T) {
+
+	assert.Equal(t, 123, 123, "they should be equal")
+	assert.NotEqual(t, 123, 456, "they should not be equal")
+}
